71e7debad749683d6e6019e927972081c72a2a2c
bsml320 <peilin.jia@gmail.com>
2019-12-20 18:20:50 -0600

    Add files via upload

--- Figures/Figure8/Figure8A.2.combine.R
+++ Figures/Figure8/Figure8A.2.combine.R
@@ -1,1 +1,79 @@
+setwd("C:/Users/pjia/UTH/road/18-VAE/analysis/V15/result.EN/dr.CCLE/04-mix/09.Expr/DAVID")
+ff = list.files("David_enrichment_plot_GOBP")
+ff = ff[grep("txt", ff)]
 
+go.bp = c()
+for(k in 1:length(ff)){
+	dat = read.delim(paste("David_enrichment_plot_GOBP/", ff[k], sep=""), as.is=T)
+	which(dat[,2] >= 10 & dat[,3] < 1e-8) -> ii
+	if(length(ii) > 0)go.bp = c(go.bp, dat[ii,1])
+}
+go.bp = unique(go.bp)
+length(go.bp)
+
+bp.mat = matrix(1, nrow=length(go.bp), ncol=length(ff))
+rownames(bp.mat) = go.bp
+f1 = gsub(".Q95.txt", "", ff)
+f1 = gsub(".high", ".H", f1)
+f1 = gsub(".low", ".L", f1)
+colnames(bp.mat) = f1
+
+for(k in 1:length(ff)){
+	dat = read.delim(paste("David_enrichment_plot_GOBP/", ff[k], sep=""), as.is=T)
+	which(dat[,2] >= 10 & dat[,3] < 1e-8) -> ii
+	if(length(ii) > 0){
+		match(dat[ii, 1], go.bp) -> idx
+		direction = 1
+		if(grepl("low", ff[k]))direction = -1
+		bp.mat[idx, k] = dat[ii, 3] * direction
+	}
+}
+
+apply(bp.mat, 1, function(u)sum( abs(u) < 1e-8)) -> check
+new.bp.mat = bp.mat[which(check >= 2), ]
+
+heatmap.2(new.bp.mat, trace="none") -> hc
+new.dat = new.bp.mat[hc$rowInd, hc$colInd]
+
+slim.new.dat = new.dat
+for(k1 in 1:nrow(new.dat)){
+	for(k2 in 1:ncol(new.dat)){
+		slim.new.dat[k1, k2] = sign(new.dat[k1, k2]) * (-log10( abs(new.dat[k1, k2]))  )
+	}
+}
+new.dat = slim.new.dat
+heatmap.2(new.dat, trace="none") -> hc
+
+scale.factor = max(abs(new.dat))
+new.dat[new.dat > 50] = 50
+new.dat = new.dat/scale.factor
+
+for(k in 1:length(ff)){
+	dat = read.delim(paste("David_enrichment_plot_GOBP/", ff[k], sep=""), as.is=T)
+	intersect(dat[,1], rownames(new.dat)) -> ii
+	if(length(ii) > 0){
+		match(intersect(dat[,1], rownames(new.dat)), rownames(new.dat)) -> idx
+		direction = 1
+		if(grepl("low", ff[k]))direction = -1
+		new.dat[idx, k] = (-log10(dat[ match(ii, dat[,1]) , 3]) ) * direction / scale.factor
+	}
+}
+corrplot(new.dat, method="ellipse")
+
+
+pdf("DAVID.BP.pdf", width=14, height=10)
+corrplot(new.dat, method="ellipse")
+dev.off()
+
+p.mat = 10^(-abs(new.dat) * scale.factor)
+pdf("DAVID.BP.pdf", width=14, height=10)
+corrplot(new.dat, p.mat = p.mat, method="ellipse", sig.level=1e-8, pch.cex = .75)
+dev.off()
+
+p.mat2 = p.mat
+p.mat2[p.mat > 1e-8] <- 1e-8
+p.mat2[p.mat < 1e-8] <- 1
+
+pdf("DAVID.BP.pdf", width=14, height=10)
+corrplot(new.dat, p.mat = p.mat2, method="ellipse", sig.level=1e-8, pch.cex = .75)
+dev.off()
---
+++ Figures/Figure8/Figure8C.R
@@ -0,0 +1,54 @@
+setwd("/work/result.EN/dr.CCLE/Figures/Figure8/")
+W5.files = dir("/work/result.EN/dr.CCLE/04-mix/09.Expr/W5/")
+
+STROMA.genes = read.table("/work/result.EN/dr.CCLE/04-mix/09.Expr/Lapatinib/STROMA.txt", as.is=T)
+STROMA.genes = as.character(STROMA.genes[,1])
+
+################################################################################## mean rank
+mean.check = c()
+for(k in 1:length(W5.files)){
+	drug = gsub("W5\\.", "", gsub(".txt", "", W5.files[k]))
+	mydata = read.table(paste("/work/result.EN/dr.CCLE/04-mix/09.Expr/W5/", W5.files[k], sep=""), header=T, as.is=T)
+	colnames(mydata)[1:3] = c("cancer", "gene", "drug")
+	
+	mydata = mydata[mydata[,1]!="BRCA" & mydata[,1]!="LGG" & mydata[,1]!="THCA",]
+	tapply(mydata[,"tvalue"], mydata[,"gene"], function(u)sum(u)/sqrt(length(u))) -> check
+	check = sort(check)
+	mean.check = rbind(mean.check, cbind(gene=names(check), check = check, drug=drug) )
+}
+
+mean.check = as.data.frame(mean.check)
+source("C:/Users/pjia/UTH/code/unfactor.R")
+mean.check = unfactor(mean.check)
+
+################################################################################################### mean data
+
+pdf("8C.pdf", height=5.5)
+drugs = unique(mean.check[,3])
+
+plot(0, 0, xlim=range(mean.check[,2]), ylim=c(0, length(drugs)+1 ), main="Average STROMA t-value", xlab="", ylab="", col="white")
+	for(kdrug in 1:length(drugs)){
+		drug = drugs[kdrug]
+		cancer.mydata = mean.check[which(mean.check[,3]==drug), ]
+		cancer.mydata = cancer.mydata[order(as.numeric(cancer.mydata[, 2])), ]
+		
+		cancer.t = as.numeric(cancer.mydata[, 2])
+		names(cancer.t) = cancer.mydata[,1]
+		
+		segments( min(mean.check[,2]), kdrug, max(mean.check[,2]), kdrug, col="grey")
+		segments( min(cancer.t), kdrug, 0, kdrug, lwd=3, col="lightgreen")
+		segments( 0, kdrug, max(cancer.t), kdrug, lwd=3, col="pink")
+		
+		which(cancer.mydata[,1] %in% STROMA.genes) -> ii
+		if(length(ii) < 1)next
+		for(point.i in ii){
+			point1.x = point.i/length(cancer.t) * (max(cancer.t) - min(cancer.t) ) + min(cancer.t)
+			point1.y = kdrug
+			segments( point1.x, kdrug-0.2, point1.x, kdrug+0.2, lwd=1, col="blue")
+		}
+		text( min(mean.check[,2]), kdrug, drug, pos=2 )
+		
+	}
+	abline(v=0, col="lightblue")
+
+dev.off()
