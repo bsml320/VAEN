87d928a110b75f780a96c6899d548e4f09dc3da2
bsml320 <peilin.jia@gmail.com>
2021-02-05 19:28:44 +0800

    Add files via upload

---
+++ Figure/FigureS10/S10.01.R
@@ -0,0 +1,132 @@
+#setwd("/path/to/VAEN/Figure/FigureS10")
+
+###################################################################################################
+
+drug.ccle = read.table(file="../../result.EN/dr.CCLE/VAEN_CCLE.MIX.pred_TCGA.txt", header=T, as.is=T, sep="\t")
+
+colnames(drug.ccle)[3:ncol(drug.ccle)] -> drugs
+cancer.types = unique(drug.ccle[,2])
+sample.type = substr(drug.ccle[,1], 14, 15)
+ss = gsub("\\.", "-", drug.ccle[,1])
+drug.ccle[,1] = ss
+
+cancer.drug.ccle = c()
+for(ct in 1:length(cancer.types)){
+	cancer = cancer.types[ct]
+	
+	type.code = "01"
+	if(cancer == "LAML"){ type.code = "03" }
+	if(cancer == "SKCM"){ type.code = "06" }
+	
+	blca.ccle = drug.ccle[which(drug.ccle[,2] == cancer & sample.type == type.code), ]
+	cancer.drug.ccle = rbind(cancer.drug.ccle, blca.ccle)
+}
+drug.ccle = cancer.drug.ccle
+
+###################################################################################################
+
+BRAF.mat = matrix(1, nrow=length(drugs), ncol=length(cancer.types))
+KRAS.mat = matrix(1, nrow=length(drugs), ncol=length(cancer.types))
+NRAS.mat = matrix(1, nrow=length(drugs), ncol=length(cancer.types))
+HRAS.mat = matrix(1, nrow=length(drugs), ncol=length(cancer.types))
+EGFR.mat = matrix(1, nrow=length(drugs), ncol=length(cancer.types))
+rownames(BRAF.mat) = drugs; colnames(BRAF.mat) = cancer.types
+rownames(KRAS.mat) = drugs; colnames(KRAS.mat) = cancer.types
+rownames(NRAS.mat) = drugs; colnames(NRAS.mat) = cancer.types
+rownames(HRAS.mat) = drugs; colnames(HRAS.mat) = cancer.types
+rownames(EGFR.mat) = drugs; colnames(EGFR.mat) = cancer.types
+
+for(k in 1:length(cancer.types)){
+	cancer = cancer.types[k]
+	
+	if(cancer == "LAML"){
+		fn = paste("../../MC3/LAML_wustl", sep="")
+	} else {
+		fn = paste("../../MC3/",cancer,"_mc3.txt", sep="")
+	}
+	
+	original.maf = read.delim(fn, as.is=T)
+	ss = original.maf[,1]; names(ss) = NULL
+
+	tapply(original.maf$gene, original.maf$sample, function(u)unique(as.character(u))) -> ss2gene
+	lapply(ss2gene, function(u){sum(u %in% c("BRAF", "KRAS", "NRAS", "HRAS", "EGFR"))}) -> check
+	check = unlist(check)
+	WT.ss = names(check[which(check==0)])
+	match(WT.ss, drug.ccle[,1]) -> ii.2; ii.2 = ii.2[!is.na(ii.2)]
+	
+	four.ii = c()
+	############
+	which(original.maf$gene == "BRAF" & original.maf$Amino_Acid_Change == "p.V600E") -> ii
+	MT.ss = unique(ss[ii])
+	match(MT.ss, drug.ccle[,1]) -> ii.1; ii.1 = ii.1[!is.na(ii.1)]; 
+	
+	if(length(ii.1) > 10){
+		p = c()
+		for(k1 in 3:ncol(drug.ccle)){
+			BRAF.mat[k1-2, k] = wilcox.test(drug.ccle[ii.1, k1], drug.ccle[ii.2, k1], alternative="greater")$p.value
+		}
+	}
+	
+	############
+	which(original.maf$gene == "KRAS" & grepl("G12",original.maf$Amino_Acid_Change)) -> ii
+	which(original.maf$gene == "KRAS" & grepl("G13",original.maf$Amino_Acid_Change)) -> ii1
+	which(original.maf$gene == "KRAS" & grepl("Q61",original.maf$Amino_Acid_Change)) -> ii2
+	ii = unique(c(ii, ii1, ii2))
+	MT.ss = unique(ss[ii])
+	match(MT.ss, drug.ccle[,1]) -> ii.1; ii.1 = ii.1[!is.na(ii.1)]; 
+	
+	if(length(ii.1) > 10){
+		for(k1 in 3:ncol(drug.ccle)){
+			KRAS.mat[k1-2, k] = t.test(drug.ccle[ii.1, k1], drug.ccle[ii.2, k1], alternative="greater")$p.value
+		}
+	}
+	
+	############
+	which(original.maf$gene == "NRAS" & grepl("G12",original.maf$Amino_Acid_Change)) -> ii
+	which(original.maf$gene == "NRAS" & grepl("G13",original.maf$Amino_Acid_Change)) -> ii1
+	which(original.maf$gene == "NRAS" & grepl("Q61",original.maf$Amino_Acid_Change)) -> ii2
+	ii = unique(c(ii, ii1, ii2))
+	MT.ss = unique(ss[ii])
+	match(MT.ss, drug.ccle[,1]) -> ii.1; ii.1 = ii.1[!is.na(ii.1)]; 
+	
+	if(length(ii.1) > 10){
+		for(k1 in 3:ncol(drug.ccle)){
+			NRAS.mat[k1-2, k] = t.test(drug.ccle[ii.1, k1], drug.ccle[ii.2, k1], alternative="greater")$p.value
+		}
+	}
+	
+	############
+	which(original.maf$gene == "HRAS" & grepl("G12",original.maf$Amino_Acid_Change)) -> ii
+	which(original.maf$gene == "HRAS" & grepl("G13",original.maf$Amino_Acid_Change)) -> ii1
+	which(original.maf$gene == "HRAS" & grepl("Q61",original.maf$Amino_Acid_Change)) -> ii2
+	ii = unique(c(ii, ii1, ii2))
+	MT.ss = unique(ss[ii])
+	match(MT.ss, drug.ccle[,1]) -> ii.1; ii.1 = ii.1[!is.na(ii.1)]; 
+	
+	if(length(ii.1) > 10){
+		for(k1 in 3:ncol(drug.ccle)){
+			HRAS.mat[k1-2, k] = t.test(drug.ccle[ii.1, k1], drug.ccle[ii.2, k1], alternative="greater")$p.value
+		}
+	}
+	
+	############
+	which(original.maf$gene == "EGFR") -> ii
+	grep("746", original.maf$Amino_Acid_Change) -> del.ii
+	grep("858", original.maf$Amino_Acid_Change) -> mut.ii
+	grep("861", original.maf$Amino_Acid_Change) -> mut2.ii
+	grep("719", original.maf$Amino_Acid_Change) -> mut3.ii
+	ii = intersect(ii, c(del.ii, mut.ii, mut2.ii, mut3.ii))
+	
+	MT.ss = unique(ss[ii])
+	match(MT.ss, drug.ccle[,1]) -> ii.1; ii.1 = ii.1[!is.na(ii.1)];
+	
+	if(length(ii.1) > 10){
+		for(k1 in 3:ncol(drug.ccle)){
+			EGFR.mat[k1-2, k] = t.test(drug.ccle[ii.1, k1], drug.ccle[ii.2, k1], alternative="greater")$p.value
+		}
+	}
+	
+}
+save(BRAF.mat, EGFR.mat, KRAS.mat, NRAS.mat, HRAS.mat, file="S10.data.RData")
+
+############################################################################################################################
---
+++ Figure/FigureS10/S10.02.R
@@ -0,0 +1,48 @@
+#setwd("/path/to/VAEN/Figure/FigureS10")
+
+library(reshape2)
+library(ggplot2)
+
+load("S10.data.RData")
+
+apply(BRAF.mat, 2, sum) -> colCheck
+clean.BRAF.mat = BRAF.mat[,colCheck!=max(colCheck)]
+colnames(clean.BRAF.mat) = paste(colnames(clean.BRAF.mat), ", BRAF", sep="")
+
+apply(NRAS.mat, 2, sum) -> colCheck
+clean.NRAS.mat = NRAS.mat[,colCheck!=max(colCheck)]
+colnames(clean.NRAS.mat) = paste(colnames(clean.NRAS.mat), ", NRAS", sep="")
+
+apply(KRAS.mat, 2, sum) -> colCheck
+clean.KRAS.mat = KRAS.mat[,colCheck!=max(colCheck)]
+colnames(clean.KRAS.mat) = paste(colnames(clean.KRAS.mat), ", KRAS", sep="")
+
+colnames(HRAS.mat) = colnames(BRAF.mat)
+apply(HRAS.mat, 2, sum) -> colCheck
+clean.HRAS.mat = HRAS.mat[,colCheck!=max(colCheck)]
+colnames(clean.HRAS.mat) = paste(colnames(clean.HRAS.mat), ", HRAS", sep="")
+
+cbind(clean.BRAF.mat, clean.NRAS.mat, clean.KRAS.mat, clean.HRAS.mat, EGFR.mat[, "LUAD"]) -> x
+colnames(x)[ncol(x)] = "LUAD, EGFR"
+x = x[, sort(colnames(x))]
+
+dat4g <- melt(as.matrix(x) )
+dat4g[,3] = as.numeric( sapply(dat4g[,3], function(u)format(as.numeric(u), digits=2)) )
+cutoff = 0.05/24/33
+
+p5 <- ggplot(data = dat4g,aes(x=Var1,y=Var2)) + 
+  geom_tile(aes(fill=value),colour='white') + 
+  geom_text(aes(label=value),vjust=0.5, size=2) + labs(title="CCLE, catenated") +
+  scale_fill_gradientn(colours=c("red","pink","white"),
+                       values=c(0,cutoff,1),
+                       guide = "none", name="-log10(p)", na.value = "white") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 8),
+		axis.text.y = element_text(size = 8),
+		plot.title = element_text(hjust = 0.5)
+		) 
+
+pdf("S10.pdf", width=8, height=5)
+print(p5)
+dev.off()
