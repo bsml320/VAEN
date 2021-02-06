934a5e11639f512efc315c714a69ec9169de39c4
bsml320 <peilin.jia@gmail.com>
2021-02-06 13:13:21 +0800

    Add files via upload

--- Figure/Figure5/GSE20194/01.getGEO.R
+++ Figure/Figure5/GSE20194/01.getGEO.R
@@ -1,1 +1,39 @@
-  
+setwd("/path/to/VAEN/Figure/Figure5/GSE20194")
+library(GEOquery)
+library(limma)
+
+###################################################################################################
+# code chunk: GSE20194
+###################################################################################################
+rm(list=ls())
+
+gse <- getGEO("GSE20194", GSEMatrix = TRUE)
+exprs(gse[[1]]) -> datExpr0
+
+########## section 1
+
+library('preprocessCore')
+normalize.quantiles(datExpr0) -> datExprLQ
+dimnames(datExprLQ) = dimnames(datExpr0)
+
+########## section 1 end
+
+########## section 2
+
+gpl = getGEO("GPL96")
+Table(gpl) -> anno
+
+grep("/", anno[,11]) -> check
+anno = anno[-check,]
+anno = anno[anno[,11]!="", ]
+
+
+datExprLQ = datExprLQ[which(rownames(datExprLQ) %in% anno[,1]),]
+match(rownames(datExprLQ), anno[,1]) -> ii
+symbol = anno[ii, 11]
+
+apply(datExprLQ, 2, function(u)tapply(u, symbol, median)) -> expr.mat
+
+pData(gse[[1]]) -> pheno.anno
+
+save(expr.mat, pheno.anno, file="GSE20194.RData")
---
+++ Figure/Figure5/GSE20194/02.GSE20194.VAEN.R
@@ -0,0 +1,73 @@
+setwd("/path/to/VAEN/Figure/Figure5/GSE20194")
+
+### key genes
+cur.genes = read.table("/path/to/VAEN/result/key.genes.txt", as.is=T)
+cur.genes = cur.genes[,1]
+
+###
+load("GSE20194.RData")
+val.RPKM = expr.mat
+
+match(cur.genes, rownames(val.RPKM)) -> ii
+
+#> summary(ii)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's
+#      2    3081    5960    6082    9145   12546    1567
+
+
+new.RPKM = val.RPKM[ii, ]
+apply(new.RPKM, 2, function(u){u[is.na(u)] = 0; u} ) -> raw.RPKM
+rownames(raw.RPKM) = cur.genes
+
+
+rank.GSE20194.gene.mat = apply(raw.RPKM, 2, function(u)rank(u)/length(u))
+rank.GSE20194.gene.mat = apply(rank.GSE20194.gene.mat, 1, function(u){
+	u[which(u==1)] = 6162.5/6163
+	u
+})
+
+scaled.GSE20194.gene.mat = apply(rank.GSE20194.gene.mat, 2, function(u){qnorm(u)} )
+
+##########################################################################
+write.table(scaled.GSE20194.gene.mat, file=paste("GSE20194.RANK.tsv", sep=""), row.names=T, quote=F, sep="\t")
+##########################################################################
+
+
+##########################################################################
+##########################################################################
+
+load("/path/to/VAEN/result.EN/dr.CCLE/dr.CCLE.A.models.RData")
+drug = "Paclitaxel"
+res.list = dr.ccle.models[[drug]]
+fit <- res.list$model
+best.index = res.list[[ "best_index" ]]
+
+#cmd = paste("python3 GSE20194.predict_VAE.py ", k, sep="")
+#system(cmd)
+
+GSE20194.pred = read.table(paste(best.index, ".GSE20194.latent.tsv", sep=""), header=T, sep="\t", as.is=T)
+GSE20194.probabilities = predict(fit, as.matrix(GSE20194.pred[,-1]), s = 'lambda.min')
+GSE20194.pred.mat = cbind(GSE20194.pred[,1], GSE20194.probabilities)
+colnames(GSE20194.pred.mat) = c("Sample", drug)
+
+write.table(GSE20194.pred.mat, file=paste("CCLE.A.pred_GSE20194.txt", sep=""), quote=F, sep="\t", row.names=FALSE)
+
+
+##########################################################################
+##########################################################################
+
+load("/path/to/GitHub/result.EN/dr.GDSC/dr.GDSC.A.models.RData")
+drug = "Erlotinib"
+res.list = dr.gdsc.models[[drug]]
+fit <- res.list$model
+best.index = res.list[[ "best_index" ]]
+
+#cmd = paste("python3 GSE20194.predict_VAE.py ", k, sep="")
+#system(cmd)
+
+GSE20194.pred = read.table(paste(best.index, ".GSE20194.latent.tsv", sep=""), header=T, sep="\t", as.is=T)
+GSE20194.probabilities = predict(fit, as.matrix(GSE20194.pred[,-1]), s = 'lambda.min')
+GSE20194.pred.mat = cbind(GSE20194.pred[,1], GSE20194.probabilities)
+colnames(GSE20194.pred.mat) = c("Sample", drug)
+
+write.table(GSE20194.pred.mat, file=paste("GDSC.A.pred_GSE20194.txt", sep=""), quote=F, sep="\t", row.names=FALSE)
