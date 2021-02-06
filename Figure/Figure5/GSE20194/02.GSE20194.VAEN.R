a26975f508e072fb4088ca6c41c0044512f63426
bsml320 <peilin.jia@gmail.com>
2021-02-06 13:58:15 +0800

    Add files via upload

--- Figure/Figure5/GSE20194/02.GSE20194.VAEN.R
+++ Figure/Figure5/GSE20194/02.GSE20194.VAEN.R
@@ -28,13 +28,13 @@ rank.GSE20194.gene.mat = apply(rank.GSE20194.gene.mat, 1, function(u){
 
 scaled.GSE20194.gene.mat = apply(rank.GSE20194.gene.mat, 2, function(u){qnorm(u)} )
 
-##########################################################################
 write.table(scaled.GSE20194.gene.mat, file=paste("GSE20194.RANK.tsv", sep=""), row.names=T, quote=F, sep="\t")
 ##########################################################################
 
 
 ##########################################################################
 ##########################################################################
+library("Matrix")
 
 load("/path/to/VAEN/result.EN/dr.CCLE/dr.CCLE.A.models.RData")
 drug = "Paclitaxel"
@@ -42,10 +42,13 @@ res.list = dr.ccle.models[[drug]]
 fit <- res.list$model
 best.index = res.list[[ "best_index" ]]
 
-#cmd = paste("python3 GSE20194.predict_VAE.py ", k, sep="")
-#system(cmd)
+##########################################################################
+# Go to a shell, and run VAE.prediction.py using best.index obtained above
+# python3 VAE.prediction.py <best.index> <path/to/GSE20194.RANK.tsv> </path/to/VAEN/result/>
+# After finish the above python code, a new file will be generated: GSE20194.RANK.<best.index>.latent.tsv
+##########################################################################
 
-GSE20194.pred = read.table(paste(best.index, ".GSE20194.latent.tsv", sep=""), header=T, sep="\t", as.is=T)
+GSE20194.pred = read.table(paste("GSE20194.RANK.",best.index,".latent.tsv", sep=""), header=T, sep="\t", as.is=T)
 GSE20194.probabilities = predict(fit, as.matrix(GSE20194.pred[,-1]), s = 'lambda.min')
 GSE20194.pred.mat = cbind(GSE20194.pred[,1], GSE20194.probabilities)
 colnames(GSE20194.pred.mat) = c("Sample", drug)
@@ -56,7 +59,7 @@ write.table(GSE20194.pred.mat, file=paste("CCLE.A.pred_GSE20194.txt", sep=""), q
 ##########################################################################
 ##########################################################################
 
-load("/path/to/GitHub/result.EN/dr.GDSC/dr.GDSC.A.models.RData")
+load("/path/to/VAEN/result.EN/dr.GDSC/dr.GDSC.A.models.RData")
 drug = "Erlotinib"
 res.list = dr.gdsc.models[[drug]]
 fit <- res.list$model
