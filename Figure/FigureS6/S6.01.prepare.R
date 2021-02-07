7b16648316d9cd54e09fd89c05510a52b913a150
bsml320 <peilin.jia@gmail.com>
2021-02-07 18:36:58 +0800

    Add files via upload

--- Figure/FigureS6/S6.01.prepare.R
+++ Figure/FigureS6/S6.01.prepare.R
@@ -1,1 +1,108 @@
-  
+setwd("/path/to/VAEN/Figure/FigureS6")
+
+raw.mut.mat = read.delim("../../DATA/CCLE/CCLE_DepMap_18q3_maf_20180718.txt", as.is=T, sep="\t", header=T)
+raw.mut.mat = raw.mut.mat[raw.mut.mat$Variant_Type!="ONP",]
+
+silence.type = c("3'UTR", "5'Flank", "5'UTR", "De_novo_Start_OutOfFrame", "IGR", "Intron", "Nonstop_Mutation", "Silent", "Start_Codon_Del", "Start_Codon_Ins", "Start_Codon_SNP", "Stop_Codon_Del", "Stop_Codon_Ins")
+mut.mat = raw.mut.mat[!raw.mut.mat$Variant_Classification %in% silence.type, ]
+mut.mat = mut.mat[mut.mat[,2]!=0,]
+genes = unique(mut.mat[,1])
+
+#######################################
+load("../../result.EN/dr.CCLE/01/1.CCLE.model.list.RData")
+ccle.model.list = model.list
+ccle.obsd = c()
+for(k in 1:length(ccle.model.list)){
+	res.list = ccle.model.list[[k]]
+	drug = names(ccle.model.list)[k]
+	Ys = res.list$Ys
+	
+	ccle.obsd = cbind(ccle.obsd, Ys[,1])
+}
+colnames(ccle.obsd) = names(ccle.model.list)
+#######################################
+
+ccle.pred      = read.table(file="../../result.EN/dr.CCLE/VAEN_CCLE.A.pred_CCLE.txt", header=T, as.is=T, sep="\t")
+ccle.pred.full = read.table(file="../../result.EN/dr.CCLE/VAEN_CCLE.A.pred_CCLE.full.txt", header=T, as.is=T, sep="\t")
+
+###################################################################################################
+sapply(rownames(ccle.obsd), function(u){
+	strsplit(u, split="\\.")[[1]] -> v; 
+	which(v == "ACH") -> ii
+	paste(v[ii:(ii+1)], collapse="-")
+}) -> ccle.obsd.ss
+names(ccle.obsd.ss) = NULL
+rownames(ccle.obsd) = ccle.obsd.ss
+
+obsd.mut.mat = mut.mat[mut.mat[, "Broad_ID"] %in% ccle.obsd.ss, ]
+### observed
+
+genes = c("EGFR", "BRAF", "KRAS", "NRAS")
+for(gene in genes){
+	dr.BRAF = c()
+	#gene = "BRAF"
+	gene.mutation = obsd.mut.mat[obsd.mut.mat[,1]==gene, ]
+	MT.ss = unique(gene.mutation[, "Broad_ID"])
+	
+	for(kdrug in 1:ncol(ccle.obsd)){
+		drug = colnames(ccle.obsd)[kdrug]
+		Y = ccle.obsd[, kdrug]
+		names(Y) = rownames(ccle.obsd)
+		Y = Y[Y!=-9]
+		MT.ss = intersect(MT.ss, names(Y))
+		dr.BRAF = rbind(dr.BRAF, cbind(drug=drug, dr=Y, mutation=ifelse(names(Y) %in% MT.ss, 1, 0), grp="obsd" ) )
+	}
+
+
+	###################################################################################################
+	sapply(ccle.pred[,1], function(u){
+		strsplit(u, split="\\.")[[1]] -> v; 
+		which(v == "ACH") -> ii
+		paste(v[ii:(ii+1)], collapse="-")
+	}) -> ccle.pred.ss
+	names(ccle.pred.ss) = NULL
+	rownames(ccle.pred) = ccle.pred.ss
+
+	pred.mut.mat = mut.mat[mut.mat[, "Broad_ID"] %in% ccle.pred.ss, ]
+	### observed
+	#gene = "BRAF"
+	gene.mutation = pred.mut.mat[pred.mut.mat[,1]==gene, ]
+	MT.ss = unique(gene.mutation[, "Broad_ID"])
+	if(length(MT.ss) < 5)next
+	
+	for(kdrug in 2:ncol(ccle.pred)){
+		drug = colnames(ccle.pred)[kdrug]
+		Y = ccle.pred[, kdrug]
+		names(Y) = rownames(ccle.pred)
+		Y = Y[Y!=-9]
+		MT.ss = intersect(MT.ss, names(Y))
+		dr.BRAF = rbind(dr.BRAF, cbind(drug=drug, dr=Y, mutation=ifelse(names(Y) %in% MT.ss, 1, 0), grp="pred" ) )
+	}
+	
+	###################################################################################################
+	sapply(ccle.pred.full[,1], function(u){
+		strsplit(u, split="\\.")[[1]] -> v; 
+		which(v == "ACH") -> ii
+		paste(v[ii:(ii+1)], collapse="-")
+	}) -> ccle.pred.ss
+	names(ccle.pred.ss) = NULL
+	rownames(ccle.pred.full) = ccle.pred.ss
+
+	pred.full.mut.mat = mut.mat[mut.mat[, "Broad_ID"] %in% ccle.pred.ss, ]
+
+	### observed
+	#gene = "BRAF"
+	gene.mutation = pred.full.mut.mat[pred.full.mut.mat[,1]==gene, ]
+	MT.ss = unique(gene.mutation[, "Broad_ID"])
+	
+	for(kdrug in 2:ncol(ccle.pred.full)){
+		drug = colnames(ccle.pred.full)[kdrug]
+		Y = ccle.pred.full[, kdrug]
+		names(Y) = rownames(ccle.pred.full)
+		Y = Y[Y!=-9]
+		MT.ss = intersect(MT.ss, names(Y))
+		dr.BRAF = rbind(dr.BRAF, cbind(drug=drug, dr=Y, mutation=ifelse(names(Y) %in% MT.ss, 1, 0), grp="full" ) )
+	}
+
+	save(dr.BRAF, file=paste("dr.",gene,".RData", sep=""))
+}
---
+++ Figure/FigureS6/S6.02.plot.R
@@ -0,0 +1,128 @@
+setwd("/path/to/VAEN/Figure/FigureS6")
+library(ggplot2)
+library(ggpubr)
+
+######################################################################################
+load("dr.BRAF.RData")
+write.table(dr.BRAF, file="dr.BRAF.txt", row.names=F, quote=F, sep="\t")
+dr.BRAF = read.table("dr.BRAF.txt", as.is=T, header=T)
+dr.BRAF$mutation = ifelse(dr.BRAF$mutation == 0, "WT", "MT")
+
+dr.BRAF$grp -> group
+group[which(group == "obsd")] = "Set 1"
+group[which(group == "pred")] = "Set 2"
+group[which(group == "full")] = "Set 3"
+dr.BRAF$grp = group
+
+dr.BRAF[which(dr.BRAF[,1]=="X17.AAG"),1] = "17-AAG"
+gsub("\\.", "-", dr.BRAF[,1]) -> ss
+dr.BRAF[,1] = ss
+
+dat = dr.BRAF[dr.BRAF[,1] %in% c("AZD6244", "PD-0325901", "PLX4720", "RAF265"), ]
+
+give.n <- function(x){
+   return(c(y = min(x) , label = length(x)))
+}
+
+pp.list = list()
+for(drug in c("AZD6244", "PD-0325901", "PLX4720", "RAF265")){
+	dat = dr.BRAF[dr.BRAF[,1] == drug, ]
+	dat$mutation = as.factor(dat$mutation)
+	
+	if(drug == "AZD6244"){
+		p = ggplot(dat, aes(x=grp, y=dr, fill=mutation)) + geom_boxplot(position=position_dodge(0.8), outlier.shape = NA) + geom_point(size=1, stroke=.5, alpha=0.2, position = position_jitterdodge(dodge.width = 0.8)) +
+			ggtitle( paste(drug, ", BRAF", sep="") ) + xlab("") + ylab("ActArea") + guides(fill=FALSE) + 
+			theme(text = element_text(size=8), axis.text.x=element_text(color="black", size = 8),axis.text.y=element_text(color="black", size = 8), plot.margin=unit(c(1,1,1,1), "mm"), plot.title = element_text(size = 8, hjust = 0.5))
+		p1 = p + stat_compare_means(aes(group = mutation), method = "t.test", label = "p.format", label.x = 0.8, size=3) +
+		     stat_summary(fun.data = give.n, geom = "text", size=3)
+	} else {
+		p = ggplot(dat, aes(x=grp, y=dr, fill=mutation)) + geom_boxplot(position=position_dodge(0.8), outlier.shape = NA) + geom_point(size=1, stroke=.5, alpha=0.2, position = position_jitterdodge(dodge.width = 0.8)) +
+		ggtitle( paste(drug, ", BRAF", sep="") ) + xlab("") + ylab("ActArea") + guides(fill=FALSE) + 
+		theme(text = element_text(size=8), axis.text.x=element_text(color="black", size = 8),axis.text.y=element_text(color="black", size = 8), plot.margin=unit(c(1,1,1,1), "mm"), plot.title = element_text(size = 8, hjust = 0.5))
+		p1 = p + stat_compare_means(aes(group = mutation), method = "t.test", label = "p.format", label.x = 0.8, size=3) + 
+		     stat_summary(fun.data = give.n, geom = "text", size=3)
+	}
+	pp.list[[paste("BRAF", drug, sep="_")]] = p1
+}
+rm(list=setdiff(ls(), c("pp.list", give.n)))
+######################################################################################
+
+load("dr.NRAS.RData")
+write.table(dr.BRAF, file="dr.NRAS.txt", row.names=F, quote=F, sep="\t")
+dr.NRAS = read.table("dr.NRAS.txt", as.is=T, header=T)
+dr.NRAS$mutation = ifelse(dr.NRAS$mutation == 0, "WT", "MT")
+
+give.n <- function(x){
+   return(c(y = min(x) , label = length(x)))
+}
+
+dr.NRAS$grp -> group
+group[which(group == "obsd")] = "Set 1"
+group[which(group == "pred")] = "Set 2"
+group[which(group == "full")] = "Set 3"
+dr.NRAS$grp = group
+
+dr.NRAS[which(dr.NRAS[,1]=="X17.AAG"),1] = "17-AAG"
+gsub("\\.", "-", dr.NRAS[,1]) -> ss
+dr.NRAS[,1] = ss
+
+dat = dr.NRAS[dr.NRAS[,1] %in% c("AZD6244", "PD-0325901", "PLX4720", "RAF265"), ]
+
+for(drug in c("AZD6244", "PD-0325901", "PLX4720", "RAF265")){
+	dat = dr.NRAS[dr.NRAS[,1] == drug, ]
+	dat$mutation = as.factor(dat$mutation)
+
+	p = ggplot(dat, aes(x=grp, y=dr, fill=mutation)) + geom_boxplot(position=position_dodge(0.8), outlier.shape = NA) + geom_point(size=1, stroke=.5, alpha=0.2, position = position_jitterdodge(dodge.width = 0.8)) +
+		ggtitle( paste(drug, ", NRAS", sep="") ) + xlab("") + ylab("ActArea") + guides(fill=FALSE) +
+		theme(text = element_text(size=8), axis.text.x=element_text(color="black", size = 8),axis.text.y=element_text(color="black", size = 8), plot.margin=unit(c(1,1,1,1), "mm"), plot.title = element_text(size = 8, hjust = 0.5))
+	
+	p1 = p + stat_compare_means(aes(group = mutation), method = "t.test", label = "p.format", label.x = 0.8, size=3) +
+	     stat_summary(fun.data = give.n, geom = "text", size=3)
+	pp.list[[paste("NRAS", drug, sep="_")]] = p1
+}
+
+rm(list=setdiff(ls(), c("pp.list", give.n)))
+######################################################################################
+
+load("dr.KRAS.RData")
+write.table(dr.BRAF, file="dr.KRAS.txt", row.names=F, quote=F, sep="\t")
+dr.KRAS = read.table("dr.KRAS.txt", as.is=T, header=T)
+dr.KRAS$mutation = ifelse(dr.KRAS$mutation == 0, "WT", "MT")
+
+give.n <- function(x){
+   return(c(y = min(x) , label = length(x)))
+}
+
+dr.KRAS$grp -> group
+group[which(group == "obsd")] = "Set 1"
+group[which(group == "pred")] = "Set 2"
+group[which(group == "full")] = "Set 3"
+dr.KRAS$grp = group
+
+dr.KRAS[which(dr.KRAS[,1]=="X17.AAG"),1] = "17-AAG"
+gsub("\\.", "-", dr.KRAS[,1]) -> ss
+dr.KRAS[,1] = ss
+
+dat = dr.KRAS[dr.KRAS[,1] %in% c("AZD6244", "PD-0325901", "PLX4720", "RAF265"), ]
+
+for(drug in c("AZD6244", "PD-0325901", "PLX4720", "RAF265")){
+	dat = dr.KRAS[dr.KRAS[,1] == drug, ]
+	dat$mutation = as.factor(dat$mutation)
+
+	p = ggplot(dat, aes(x=grp, y=dr, fill=mutation)) + geom_boxplot(position=position_dodge(0.8), outlier.shape = NA) + geom_point(size=1, stroke=1, alpha=0.2, position = position_jitterdodge(dodge.width = 0.8)) +
+		ggtitle( paste(drug, ", KRAS", sep="") ) + xlab("") + ylab("ActArea") + guides(fill=FALSE) +
+		theme(text = element_text(size=8), axis.text.x=element_text(color="black", size = 8),axis.text.y=element_text(color="black", size = 8), plot.margin=unit(c(1,1,1,1), "mm"), plot.title = element_text(size = 8, hjust = 0.5))
+	p1 = p + stat_compare_means(aes(group = mutation), method = "t.test", label = "p.format", label.x = 0.8, size=3) +
+	stat_summary(fun.data = give.n, geom = "text", size=3)
+	pp.list[[paste("KRAS", drug, sep="_")]] = p1
+}
+
+######################################################################################
+source("../../code/multiplot.R")
+
+pdf("FigureS6.pdf", width=12, height=9)
+layout <- matrix(c(1:12), nrow = 3, byrow = TRUE)
+multiplot(plotlist = pp.list, layout = layout)
+dev.off()
+
+######################################################################################
