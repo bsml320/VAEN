e8f82f4878945ea076d442486c300437b87f49e2
bsml320 <peilin.jia@gmail.com>
2021-02-09 19:10:25 +0800

    Update 03.pCR.R

--- Figure/Figure5/GSE25055/03.pCR.R
+++ Figure/Figure5/GSE25055/03.pCR.R
@@ -1,4 +1,4 @@
-#setwd("/path/to/VAEN/Figure/Figure5/GSE25055")
+setwd("/path/to/VAEN/Figure/Figure5/GSE25055")
 library("survminer")
 library("survival")
 source("../../../code/multiplot.R")
@@ -13,12 +13,12 @@ ccle = read.table("CCLE.A.pred_GSE25055.txt", as.is=T, header=T)
 
 ### all
 dat = data.frame(cbind(Response = ccle[, "Paclitaxel"], pCR=pheno.anno[,"characteristics_ch1.21"]))
-#dat[,1] = as.numeric(as.character(dat[,1]))
+dat[,1] = as.numeric(as.character(dat[,1]))
 t.test(dat[,1] ~ dat[,2])
 
-# dat[which(dat[,2]=="dlda30_prediction: pCR"),2] = "pCR"
-# dat[which(dat[,2]=="dlda30_prediction: RD"),2] = "RD"
-# dat[,2] <- factor(dat[,2], levels = c("RD", "pCR"))
+dat[which(dat[,2]=="dlda30_prediction: pCR"),2] = "pCR"
+dat[which(dat[,2]=="dlda30_prediction: RD"),2] = "RD"
+dat[,2] <- factor(dat[,2], levels = c("RD", "pCR"))
 
 pvalue = t.test(dat[,1] ~ dat[,2])$p.value
 
@@ -32,12 +32,12 @@ gdsc = read.delim("GDSC.A.pred_GSE25055.txt", as.is=T)
 
 ### all
 dat = data.frame(cbind(Response = gdsc[, "Paclitaxel"], pCR=pheno.anno[,"characteristics_ch1.21"]))
-#dat[,1] = as.numeric(as.character(dat[,1]))
+dat[,1] = as.numeric(as.character(dat[,1]))
 t.test(dat[,1] ~ dat[,2])
 
-# dat[which(dat[,2]=="dlda30_prediction: pCR"),2] = "pCR"
-# dat[which(dat[,2]=="dlda30_prediction: RD"),2] = "RD"
-# dat[,2] <- factor(dat[,2], levels = c("RD", "pCR"))
+dat[which(dat[,2]=="dlda30_prediction: pCR"),2] = "pCR"
+dat[which(dat[,2]=="dlda30_prediction: RD"),2] = "RD"
+dat[,2] <- factor(dat[,2], levels = c("RD", "pCR"))
 
 pvalue = t.test(dat[,1] ~ dat[,2])$p.value
 
@@ -50,5 +50,3 @@ p2 = ggplot(dat, aes(x=pCR, y=Response, fill=pCR)) + geom_boxplot() +
 pdf("5G.1.CCLE.Paclitaxel.sensitive.ggplot.pdf", width=6, height=6)
 multiplot(plotlist=list(p1,p2), layout=matrix(1:2, nrow=1))
 dev.off()
-
-#############################################################
